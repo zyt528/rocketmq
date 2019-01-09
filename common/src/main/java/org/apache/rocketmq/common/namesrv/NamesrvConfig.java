ab0a88657d2e46b9ab89f47c638546391c778f85
yuntzhao <yuntzhao@ebay.com>
2019-01-09 15:36:25 +0800

    RocketMQ Internals: chapter 1
    RocketMQ Internals: chapter 1

--- common/src/main/java/org/apache/rocketmq/common/namesrv/NamesrvConfig.java
+++ common/src/main/java/org/apache/rocketmq/common/namesrv/NamesrvConfig.java
@@ -30,8 +30,10 @@ public class NamesrvConfig {
     private static final InternalLogger log = InternalLoggerFactory.getLogger(LoggerName.NAMESRV_LOGGER_NAME);
 
     private String rocketmqHome = System.getProperty(MixAll.ROCKETMQ_HOME_PROPERTY, System.getenv(MixAll.ROCKETMQ_HOME_ENV));
-    private String kvConfigPath = System.getProperty("user.home") + File.separator + "namesrv" + File.separator + "kvConfig.json";
-    private String configStorePath = System.getProperty("user.home") + File.separator + "namesrv" + File.separator + "namesrv.properties";
+    private String kvConfigPath = System.getProperty("ROCKETMQ_HOME") + File.separator + "namesrv" + File.separator + "kvConfig.json";
+//    private String kvConfigPath = System.getProperty("user.home") + File.separator + "namesrv" + File.separator + "kvConfig.json";
+    private String configStorePath = System.getProperty("ROCKETMQ_HOME") + File.separator + "namesrv" + File.separator + "namesrv.properties";
+//    private String configStorePath = System.getProperty("user.home") + File.separator + "namesrv" + File.separator + "namesrv.properties";
     private String productEnvName = "center";
     private boolean clusterTest = false;
     private boolean orderMessageEnable = false;
